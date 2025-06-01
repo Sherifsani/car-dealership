c103ee53d65e86e42a9050ce89b24693bba26e0c
Sherif Sani <sanisherif11@gmail.com>
2025-06-01 18:47:00 +0100

    added auth middleware

---
+++ src/middlewares/auth-middleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+declare global {
+    namespace Express {
+        interface Request {
+            userInfo?: string | JwtPayload;
+        }
+    }
+}
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: 'Unauthorized',
+        });
+    }
+
+    try {
+        const secret = process.env.JWT_SECRET_KEY as string;
+        const decodedToken = jwt.verify(token, secret);
+        req.userInfo = decodedToken;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: 'Invalid token',
+        });
+    }
+};
+
+export default authMiddleware;
