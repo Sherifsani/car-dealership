33b6398770f20c31e0efb77bee8c360dcb196133
Sherif Sani <sanisherif11@gmail.com>
2025-06-01 18:01:31 +0100

    added routes for register, login and change-password

--- src/routes/auth/auth-routes.ts
+++ src/routes/auth/auth-routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import {register} from "../../controllers/auth/auth-controller";
+import {register, login, changePassword} from "../../controllers/auth/auth-controller";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/register", register)
+authRoutes.post("/login", login)
+authRoutes.post("/change-password", changePassword)
 
 
 export default authRoutes;
